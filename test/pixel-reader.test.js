4eb953d4319c14a2422a8c7cb4969a597eb6b556
Paige Gorry <paigeegorry@gmail.com>
2019-07-10 11:39:08 -0700

    pixel-reader tests passing, read method written for the pixel reader class

--- test/pixel-reader.test.js
+++ test/pixel-reader.test.js
@@ -2,35 +2,56 @@ const PixelReader = require('../lib/pixel-reader');
 
 describe('Pixel Reader', () => {
 
-    it('reads pixel from buffer', done => {
-        const reader = new PixelReader({ bitsPerPixel: 24 });
+  it('reads pixel from buffer', done => {
+    const reader = new PixelReader({ bitsPerPixel: 24 });
 
-        const colors = [];
+    const colors = [];
 
-        // TODO: subscribe to reader "color" event and push into `colors` array.
-        // A "color" object should look like:
-        // {
-        //     offset: <offset from the start of buffer passed to PixelReader>,
-        //     r: <red color value>,
-        //     g: <green color value>,
-        //     b: <blue color value>,
-        // }
-
-        reader.on('end', () => {
-            // write deepEqual assertion for colors versus the
-            // expect().toEqual()
-            // expected rgb color objects
-
-            // Don't forget to call done()!
-        });
-
-        // Create a buffer with known data for your colors
-        const buffer = Buffer.alloc(24 * 3); // for three pixels
-        // TODO: fill buffer with byte values that match your
-        // expected test colors
-
-        // Call read method with your buffer
-        reader.read(buffer);
+    reader.on('color', color => {
+      colors.push(color);
     });
+
+    reader.on('end', () => {
+      expect(colors[0]).toEqual({
+        offset: 0,
+        r: 0xFF,
+        g: 0x00,
+        b: 0x00
+      });
+      expect(colors[1]).toEqual({
+        offset: 3,
+        r: 0x00,
+        g: 0xFF,
+        b: 0x00
+      });
+      expect(colors[2]).toEqual({
+        offset: 6,
+        r: 0x00,
+        g: 0x00,
+        b: 0xFF
+      });
+
+      done();
+    });
+
+    const buffer = Buffer.alloc(9); 
+
+    //write a red pixel to the buffer
+    buffer.writeUInt8(0xFF, 0);
+    buffer.writeUInt8(0x00, 1);
+    buffer.writeUInt8(0x00, 2);
+
+    //write a green pixel to the buffer
+    buffer.writeUInt8(0x00, 3);
+    buffer.writeUInt8(0xFF, 4);
+    buffer.writeUInt8(0x00, 5);
+
+    //write a blue pixel to the buffer
+    buffer.writeUInt8(0x00, 6);
+    buffer.writeUInt8(0x00, 7);
+    buffer.writeUInt8(0xFF, 8);
+
+    reader.read(buffer);
+  });
 
 });
