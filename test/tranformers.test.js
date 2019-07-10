f9426c84dbbc500348b17684607b1d88e83f97cd
Paige Gorry <paigeegorry@gmail.com>
2019-07-10 11:06:01 -0700

    invert transformer and grayscale transformer tests passing.

--- test/tranformers.test.js
+++ test/tranformers.test.js
@@ -3,36 +3,35 @@ const grayscale = require('../lib/grayscale-transformer');
 
 describe('transformers', () => {
 
-    it('invert', () => {
-        // HINT: invert subtracts each value from 255
-        const transformed = invert({
-            r: 34,
-            g: 100,
-            b: 205
-        });
-
-        expect(transformed).toEqual({
-            r: 221,
-            g: 155,
-            b: 50
-        });
+  it('invert', () => {
+    const transformed = invert({
+      r: 34,
+      g: 100,
+      b: 205
     });
 
-    it('grayscale', () => {
-        // HINT: grayscale assigns the average of all three colors
-        // as the new value for each color
-        const transformed = grayscale({
-            r: 34,
-            g: 100,
-            b: 205
-        });
+    expect(transformed).toEqual({
+      r: 221,
+      g: 155,
+      b: 50
+    });
+  });
 
-        expect(transformed).toEqual({
-            r: 113,
-            g: 113,
-            b: 113
-        });
+  it('grayscale', () => {
+    // HINT: grayscale assigns the average of all three colors
+    // as the new value for each color
+    const transformed = grayscale({
+      r: 34,
+      g: 100,
+      b: 205
     });
 
-    // TODO: add a third transformer (you'll need to add the module and require!) and test
+    expect(transformed).toEqual({
+      r: 113,
+      g: 113,
+      b: 113
+    });
+  });
+
+  // TODO: add a third transformer (you'll need to add the module and require!) and test
 });
